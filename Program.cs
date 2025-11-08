74706f444f7d15cb2c1facbfc545c7038ebf83fb
Bhavya <143334304+bhavya-parmar@users.noreply.github.com>
2025-11-08 20:51:15 +0530

    Added the codes

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿//using System;
+
+//class Program
+//{
+//    private int data;
+//    private static int counter = 0;
+
+//    public Program()
+//    {
+//        data = 0;
+//        counter++;
+//        Console.WriteLine($"Constructor Called. Active objects: {counter}");
+//    }
+
+//    ~Program()
+//    {
+//        counter--;
+//        Console.WriteLine($"Object Destroyed. Active objects: {counter}");
+//    }
+
+//    public void set_data(int x)
+//    {
+//        this.data = x;
+//    }
+
+//    public void show_data()
+//    {
+//        Console.WriteLine($"Data = {this.data}");
+//    }
+
+//    private static void MakeAndUseObject()
+//    {
+//        Console.WriteLine("Creating 3 objects...\n");
+//        Program p1 = new Program();
+//        Program p2 = new Program();
+//        Program p3 = new Program();
+
+//        Console.WriteLine("\nSetting data for each object...\n");
+//        p1.set_data(10);
+//        p2.set_data(20);
+//        p3.set_data(30);
+
+//        Console.WriteLine("Displaying data for each object...\n");
+//        p1.show_data();
+//        p2.show_data();
+//        p3.show_data();
+//    }
+
+//    public static void Main(string[] args)
+//    {
+//        MakeAndUseObject();
+
+//        Console.WriteLine("\nCleaning up...\n");
+//        GC.Collect();
+//        GC.WaitForPendingFinalizers();
+//        GC.Collect();
+//        Console.WriteLine($"Active objects remaining: {counter}");
+//        Console.ReadKey(); // to see output before console window closes
+//    }
+//}
---
+++ Program2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+class Vehicle
+{
+    protected int speed;
+    protected int fuel;
+
+    public Vehicle(int s, int f)
+    {
+        speed = s;
+        fuel = f;
+    }
+
+    public virtual void ShowInfo()
+    {
+        Console.WriteLine($"Speed: {speed}, Fuel: {fuel}");
+    }
+
+    public virtual void Drive()
+    {
+        fuel -= 5;
+        Console.WriteLine("Vehicle is moving...");
+    }
+}
+
+class Car : Vehicle
+{
+    private int passengers;
+
+    public Car(int s, int f, int p) : base(s, f)
+    {
+        passengers = p;
+    }
+
+    public override void Drive()
+    {
+        fuel -= 10;
+        Console.WriteLine("Car is moving with passenger.");
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Speed: {speed}, Fuel: {fuel}, Passengers: {passengers}");
+    }
+}
+
+class Truck : Vehicle
+{
+    private int cargoWeight;
+
+    public Truck(int s, int f, int c) : base(s, f)
+    {
+        cargoWeight = c;
+    }
+
+    public override void Drive()
+    {
+        fuel -= 15;
+        Console.WriteLine("Truck is moving with cargo.");
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Speed: {speed}, Fuel: {fuel}, Cargo: {cargoWeight}");
+    }
+}
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Vehicle(60, 100);
+        vehicles[1] = new Car(80, 120, 4);
+        vehicles[2] = new Truck(70, 150, 2000);
+
+        foreach (Vehicle v in vehicles)
+        {
+            v.Drive();
+            v.ShowInfo();
+        }
+        Console.ReadKey(); // to see output before console window closes
+    }
+}
